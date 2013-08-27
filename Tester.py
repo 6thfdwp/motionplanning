24586d13f9f6bbe04c1f65eced881bdc150ade52
unknown <s4255836@GS122-2172.labs.eait.uq.edu.au>
2013-08-27 12:52:53 +1000

    Tester

--- Tester.py
+++ Tester.py
@@ -1,7 +1,7 @@
 from motionplanning.ASVPlanner import ASVPlanner
 
-testcases = ['3ASV-t1.txt', '3ASV-variable-x4.txt'] 
-tuningPara = [ [(20,0.1)], [(10,0.2), (30,0.15)] ]
+testcases = ['3ASV-t1.txt', '3ASV-variable-x4.txt', '3ASV-fixed-x4.txt', '7ASV-x4.txt'] 
+tuningPara = [ [(150,0.1), (150,0.2), (200,0.1) (200,0.2)], [(150,0.1), (150,0.2), (200,0.1) (200,0.2)], [(150,0.1), (150,0.2), (200,0.1) (200,0.2)], [(150,0.1), (150,0.2), (200,0.1) (200,0.2)] ]
 
 for i, case in enumerate(testcases):
     paras = tuningPara[i]
